2a9ddda49654df0f62f93f9b338833c42e207ffb
medhatmebed <medhat.mebed@gmail.com>
2023-12-23 00:19:03 +0200

    TextBootcamp and ShapesBootcamp

---
+++ SwiftUI Bootcamp/TextBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextBootcamp.swift
+//  SwiftUI Bootcamp
+//
+//  Created by Medhat Mebed on 12/22/23.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
