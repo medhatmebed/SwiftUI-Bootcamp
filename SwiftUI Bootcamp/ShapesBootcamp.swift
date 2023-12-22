cd8382b41926d401793fdf46a9c630c103c987e0
medhatmebed <medhat.mebed@gmail.com>
2023-12-23 00:19:50 +0200

    TextBootcamp and ShapesBootcamp

--- SwiftUI Bootcamp/ShapesBootcamp.swift
+++ SwiftUI Bootcamp/ShapesBootcamp.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ShapesBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+     //   Circle()
+        //Ellipse()
+        //Capsule(style: .circular)
+        Rectangle()
+            //.fill(Color.red)
+           // .foregroundColor(.green)
+          // .stroke()
+          //  .stroke(Color.red, lineWidth: 5.0)
+          //  .stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [30]))
+            //.trim(from: 0.2, to: 0.8)
+          //  .frame(width: 200, height: 180)
     }
 }
 
--- SwiftUI Bootcamp/TextBootcamp.swift
+++ SwiftUI Bootcamp/TextBootcamp.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct TextBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("hello, world!".capitalized)
+//            .font(.body)
+//            .fontWeight(.semibold)
+            //.bold()
+            //.underline()
+//            .underline(true, color: .red)
+           // .strikethrough(true, color: .red)
+           // .font(.system(size: 32, weight: .heavy, design: .rounded))
+            //.baselineOffset(10.0)
+            //.kerning(2.0)
+            //.multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 200, alignment: .trailing)
+            .minimumScaleFactor(10.0)
     }
 }
 
